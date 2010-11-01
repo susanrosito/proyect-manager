7eda0ca1fb3f214ba578f4a485749bbaf055daba
rosito.susana <rosito.susana@182c811d-240a-0a5f-e297-eb3b1cc4cf16>
2010-11-01 03:26:14 +0000

    Modifique tanto la tarea simple, como tarea abstracta, agregando comentarios y metodos que faltaban

--- trunk/TPFinal/src/usuarioMiembroYFecha/Fecha.java
+++ trunk/TPFinal/src/usuarioMiembroYFecha/Fecha.java
@@ -1,63 +1,55 @@
 package usuarioMiembroYFecha;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Fecha {
-	
+
 	Date fecha;
-	
-	public Fecha()
-	{
-			
-		this.fecha=new Date();
-		
+
+	public Fecha() {
+
+		this.fecha = new Date();
+
 	}
-	
-	public Fecha(String yyyyMMdd)
-	{
+
+	public Fecha(String yyyyMMdd) {
 		SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
 		String strFecha = yyyyMMdd;
-		this.fecha=null;
+		this.fecha = null;
 		try {
 
 			this.fecha = formatoDelTexto.parse(strFecha);
-            
+
 		} catch (ParseException ex) {
 
-		ex.printStackTrace();
+			ex.printStackTrace();
 
-		}}
-		
-		protected boolean antesQue(Fecha cuando)
-		{
-			return this.getFecha().before(cuando.getFecha());
-			
 		}
-		
-		protected boolean DespuesQue(Fecha cuando)
-		{
-			return this.getFecha().after(cuando.getFecha());
-			
-		}
-		
-	
-		public Fecha fechaDeHoy()
-		{
-			
-			return new Fecha();
-		}
+	}
 
-	
-	
+	protected boolean antesQue(Fecha cuando) {
+		return this.getFecha().before(cuando.getFecha());
+
+	}
+
+	protected boolean DespuesQue(Fecha cuando) {
+		return this.getFecha().after(cuando.getFecha());
+
+	}
+
+	public Fecha fechaDeHoy() {
+
+		return new Fecha();
+	}
+
 	public Date getFecha() {
-			return fecha;
-		}
+		return fecha;
+	}
 
-		public void setFecha(Date fecha) {
-			this.fecha = fecha;
-		}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 }
